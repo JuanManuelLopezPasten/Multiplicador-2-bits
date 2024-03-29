87bccbad286fbceaf3deacb76218c33e75300fae
JuanManuelLopezPasten <165343225+JuanManuelLopezPasten@users.noreply.github.com>
2024-03-28 20:30:50 -0600

    Update and rename project.v to tt_um_mult_2b.v

--- src/tt_um_mult_2b.v
+++ src/tt_um_mult_2b.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_example (
+module tt_um_mult_2b (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -17,8 +17,9 @@ module tt_um_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
+    mult_2b c1 (.a(ui_in[1:0]),.b(ui_in[3:2]),.out(uo_out[3:0]));
+    assign uio_oe = 8'b00000000;
+    assign uio_out [7:0] = 8'b00000000;
+    assign uo_out [7:4] = 4'b0000;
+    
 endmodule
